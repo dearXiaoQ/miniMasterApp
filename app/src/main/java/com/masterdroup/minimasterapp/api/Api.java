53f3c881d53608defb6f9f9b1d560e6c9132afca
11473 <juner122king@gmil.com>
2017-01-04 17:13:14 +0800

    掉失备份后的第6次提交  菜谱详情页面完成 步骤部分

--- app/src/main/java/com/masterdroup/minimasterapp/api/Api.java
+++ app/src/main/java/com/masterdroup/minimasterapp/api/Api.java
@@ -9,6 +9,7 @@ import com.masterdroup.minimasterapp.model.User;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import rx.Observable;
 
