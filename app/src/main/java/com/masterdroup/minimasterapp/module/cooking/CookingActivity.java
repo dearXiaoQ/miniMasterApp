ccd5e32d757f9a7dc10fe62a2b892b121f96adfa
11473 <juner122king@gmil.com>
2017-02-14 17:57:25 +0800

    掉失备份后的第6次提交  菜谱详情页面完成 步骤部分

---
+++ app/src/main/java/com/masterdroup/minimasterapp/module/cooking/CookingActivity.java
@@ -0,0 +1,15 @@
+package com.masterdroup.minimasterapp.module.cooking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.masterdroup.minimasterapp.R;
+
+public class CookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cooking);
+    }
+}
