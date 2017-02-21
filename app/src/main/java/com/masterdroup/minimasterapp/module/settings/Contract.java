8c4f98806535416e05f2ce43b51fc9560210a59e
11473 <juner122king@gmil.com>
2017-02-21 17:51:00 +0800

    掉失备份后的第10次提交  用户头像可更换

---
+++ app/src/main/java/com/masterdroup/minimasterapp/module/settings/Contract.java
@@ -0,0 +1,41 @@
+package com.masterdroup.minimasterapp.module.settings;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+
+import com.masterdroup.minimasterapp.BasePresenter;
+import com.masterdroup.minimasterapp.BaseView;
+import com.masterdroup.minimasterapp.model.User;
+
+/**
+ * Created by 11473 on 2017/2/20.
+ */
+
+public class Contract {
+
+
+    interface Presenter extends BasePresenter {
+        ///更新信息
+        void upDate(User user, @Nullable String headUrl);
+
+        //获取用户信息
+        void getUserDate();
+
+        //启动图片选择器
+        void openImageSelector();
+    }
+
+    interface View extends BaseView<Presenter> {
+        Context onGetContext();
+
+        void putUserHeadUrl(String url);
+
+        String getUserHeadUrl();
+
+        void putUserHeadServerUrl(String url);
+
+        String getUserHeadServerUrl();
+
+        void setUserDate(User userDate);
+    }
+}
