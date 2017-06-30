913d805fa9e307ccb5d20fcb0c4867b73234dde0
dearXiaoQ <459817761@qq.com>
2017-06-30 08:50:01 +0800

    第30次提交   smartCook 获取用户自身菜谱列表

--- app/src/main/java/com/masterdroup/minimasterapp/module/settings/FeedBackActivity.java
+++ app/src/main/java/com/masterdroup/minimasterapp/module/settings/FeedBackActivity.java
@@ -4,12 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.masterdroup.minimasterapp.R;
-
+//意见反馈页面
 public class FeedBackActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_back);
+
     }
 }
